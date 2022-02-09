621dab93eb10a859e71420372eb904de70dec396
tania <tania@efaber.net>
2022-02-09 13:25:21 +0100

    Fixes documents tab position

--- app/overrides/product_document_tab.rb
+++ app/overrides/product_document_tab.rb
@@ -2,7 +2,7 @@ Deface::Override.new(
   virtual_path: 'spree/admin/shared/_product_tabs',
   name: 'admin_product_document_tab',
   # original: '10edf2223cf4fe1ebcd351beb56472f5b623ea6b',
-  insert_after: 'ul[data-hook="admin_product_tabs"] > li:nth-child(2)',
+  insert_after: '[data-hook="admin_product_tabs"] li:nth-child(2)',
   text: <<-HTML
       <% if can?(:admin, Spree::Document) && !@product.deleted? %>
       <li>
