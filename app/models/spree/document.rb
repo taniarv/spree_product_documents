e83e123759eccfd7f070de5d556b17cbe50799cb
tania <tania@efaber.net>
2022-02-09 01:33:27 +0100

    Old_attachment for paperclip, attachment for activestorage

--- app/models/spree/document.rb
+++ app/models/spree/document.rb
@@ -14,27 +14,23 @@ module Spree
                       path: ':rails_root/public/spree/products/:id/:basename.:extension',
                       convert_options: { all: '-strip -auto-orient -colorspace sRGB' }
     validates_attachment :old_attachment, presence: true, content_type: { content_type:  ALLOWED_CONTENT_TYPES}
-    validate :no_attachment_errors
-
-    validates_attachment :attachment,
-      :presence => true,
-      :content_type => { :content_type =>  ALLOWED_CONTENT_TYPES}
+    validate :no_old_attachment_errors
 
     before_post_process :skip_thumbnail_creation
 
     # if there are errors from the plugin, then add a more meaningful message
-    def no_attachment_errors
-      unless attachment.errors.empty?
+    def no_old_attachment_errors
+      unless old_attachment.errors.empty?
         # uncomment this to get rid of the less-than-useful interim messages
         # errors.clear
-        errors.add :attachment, "Paperclip returned errors for file '#{attachment_file_name}' - check ImageMagick installation or image source file."
+        errors.add :old_attachment, "Paperclip returned errors for file '#{old_attachment_file_name}' - check ImageMagick installation or image source file."
         false
       end
     end
 
     private
     def skip_thumbnail_creation
-      return false if ALLOWED_CONTENT_TYPES.include?(attachment_content_type)
+      return false if ALLOWED_CONTENT_TYPES.include?(old_attachment_content_type)
     end
   end
 end
--- spree_product_documents.gemspec
+++ spree_product_documents.gemspec
@@ -2,7 +2,7 @@
 Gem::Specification.new do |s|
   s.platform    = Gem::Platform::RUBY
   s.name        = 'spree_product_documents'
-  s.version     = '4.0.1.rc1'
+  s.version     = '4.0.1.2'
   s.summary     = 'Add documents assets to products'
   s.description = 'Currently supports PDF and ZIP files'
   s.required_ruby_version = '>= 1.9.3'
@@ -16,6 +16,7 @@ Gem::Specification.new do |s|
   s.requirements << 'none'
 
   s.add_dependency 'spree_core', '~> 4'
+  s.add_dependency 'activestorage', '~> 6'
   s.add_dependency 'spree_backend', '~> 4'
   s.add_dependency 'file_validators', '~> 3'
 
