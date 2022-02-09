84f064f73e1303a8a3d563591c123f8256256423
tania <tania@efaber.net>
2022-02-09 02:43:08 +0100

    Fixes product_document_tab override

--- app/overrides/product_document_tab.rb
+++ app/overrides/product_document_tab.rb
@@ -3,14 +3,14 @@ Deface::Override.new(
   name: 'admin_product_document_tab',
   # original: '10edf2223cf4fe1ebcd351beb56472f5b623ea6b',
   insert_after: 'ul[data-hook="admin_product_tabs"] > li:first',
-  text: "
+  text: '
     <% if can?(:admin, Spree::Document) && !@product.deleted? %>
       <li>
-        <%= link_to_with_icon 'file',
-          I18n.t('spree.admin.tab.documents'),
+        <%= link_to_with_icon "file",
+          I18n.t("spree.admin.tab.documents"),
           admin_product_documents_path(@product),
-          class: 'nav-link #{"active" if controller_name.to_sym.eql?(:documents)}' %>
+          class: "nav-link #{'active' if current == :documents}" %>
       </li>
     <% end %>
-  "
+  '
 )
